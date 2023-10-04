93e8cb82ce5608c2623aea796c9d0fa55608db20
Majsburken <annabark1@gmail.com>
2023-10-04 16:52:13 +0200

    Step 6: Conditional rendering added capital

--- src/CountryInfo.jsx
+++ src/CountryInfo.jsx
@@ -1,7 +1,7 @@
 import './CountryInfo.css'
 
-function CountryInfo({country,maxArea}) {
-    /*console.log(data)*/
+function CountryInfo({country, maxArea, detailed}) {
+    console.log(country)
 
     console.log(maxArea)
 
@@ -13,6 +13,10 @@ function CountryInfo({country,maxArea}) {
         <div style={{width:900, textAlign: 'left'}}>
             <b>{country.name.common}</b> {country.area} km<sup>2</sup>
             <div className="bar" style={{width:ratio}}></div>
+            {detailed &&     
+            <p>Capital: {country.capital}</p> 
+            }
+
         </div>
         
 
